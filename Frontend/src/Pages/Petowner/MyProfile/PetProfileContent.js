e3bf3bcad0f89102f31329084139856743c0e8d1
Pavani263 <2019cs093@stu.ucsc.lk>
2022-08-16 17:05:10 +0530

    Merge files

--- Frontend/src/Pages/Petowner/MyProfile/PetProfileContent.js
+++ Frontend/src/Pages/Petowner/MyProfile/PetProfileContent.js
@@ -16,7 +16,7 @@ import Stack from '@mui/material/Stack';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select;
+import Select from '@mui/material/Select';
 
 
 import alex from '../../../Images/alex.png'
