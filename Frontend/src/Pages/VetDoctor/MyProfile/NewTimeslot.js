f19b0dfcbe81ec8bf57a2ec1e6bacd9ed59b1832
prasadani98 <nadeesha.aluthwaththa@gmail.com>
2022-08-20 12:36:57 +0530

    Added timeslot adding to doc profile

--- Frontend/src/Pages/VetDoctor/MyProfile/NewTimeslot.js
+++ Frontend/src/Pages/VetDoctor/MyProfile/NewTimeslot.js
@@ -5,6 +5,10 @@ import Stack from '@mui/material/Stack';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 export default function NewTimeslot() {
 //choose a day
@@ -13,12 +17,13 @@ export default function NewTimeslot() {
     const chooseDay = (event: SelectChangeEvent) => {
       setDay(event.target.value);
     };
+    const [value, setValue] = React.useState<Date | null>(null);
 //choose start time
-    const [startTime, setStartTime] = React.useState('');
+    // const [startTime, setStartTime] = React.useState('');
 
-    const chooseStartTime = (event: SelectChangeEvent) => {
-      setStartTime(event.target.value);
-    };
+    // const chooseStartTime = (event: SelectChangeEvent) => {
+    //   setStartTime(event.target.value);
+    // };
 
 //choose end time
 const [endTime, setEndTime] = React.useState('');
@@ -50,22 +55,16 @@ const chooseEndTime = (event: SelectChangeEvent) => {
   </Select>
 </FormControl>
       <FormControl sx={{width:"150px"}}>
-  <InputLabel id="demo-simple-select-label">Start Time</InputLabel>
-  <Select
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={startTime}
-    label="StartTime"
-    onChange={chooseStartTime}
-  >
-    <MenuItem value={1}>Monday</MenuItem>
-    <MenuItem value={2}>Tuesday</MenuItem>
-    <MenuItem value={3}>Wednesday</MenuItem>
-    <MenuItem value={4}>Thursday</MenuItem>
-    <MenuItem value={5}>Friday</MenuItem>
-    <MenuItem value={6}>Saturday</MenuItem>
-    <MenuItem value={7}>Sunday</MenuItem>
-  </Select>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <TimePicker
+        label="Basic example"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
 </FormControl>
 
 <FormControl sx={{width:"150px"}}>
