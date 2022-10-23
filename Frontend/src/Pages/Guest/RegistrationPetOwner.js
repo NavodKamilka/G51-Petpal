02ba6edb4f3b338bb51e6be313cbd2a55da4315b
KeshaniEkanayake <dimagikeshani5@gmail.com>
2022-10-23 09:18:04 +0530

    changed logo in RegistrationPetOwner.js

--- Frontend/src/Pages/Guest/RegistrationPetOwner.js
+++ Frontend/src/Pages/Guest/RegistrationPetOwner.js
@@ -10,7 +10,7 @@ import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import axios from "axios";
-import logo from "../../Images/Logo.png";
+import logo from "../../Images/loginLogo.jpeg";
 import { addUser } from "../../redux/userApiCalls";
 import { useDispatch, useSelector } from "react-redux";
 import '../../Style/Guest/Login.css'
